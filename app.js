9bebd1f79240382a610e2eaaf403877ea418578f
Jaime Toro <zomo505@gmail.com>
2023-07-04 17:50:13 +0200

    DOOOOOOONNNNNEEEEE!

--- app.js
+++ app.js
@@ -16,7 +16,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+   
+  punkAPI.getBeers('/beers')
+  .then(beersFromApi => {
+    res.render('beers', {beersFromApi}) 
+    console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+  
+
+});
+
+app.get('/random-beer', (req, res) => {
+   
+  punkAPI.getRandom('/random-beer')
+  .then(beersFromApi => {
+    res.render('random-beer', {beersFromApi}) 
+    console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+  
+
+});
+
+app.get('/views/partials/beer.ejs', (req, res) => {
+   
+  punkAPI.getRandom('/views/partials/beer.ejs')
+  .then(randomBeer => {
+    res.render('beer.ejs', {randomBeer}) 
+    console.log('Beers from the database: ', randomBeer)
+  })
+  .catch(error => console.log(error));
+  
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
